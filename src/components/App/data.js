993c0a8b8804b3af8d3d7dc4d72fea1b63f3f4db
ILIAS RAFII <ilias3rafii@gmail.com>
2021-02-28 00:29:47 +0100

    add invoice generator to ui

---
+++ src/components/App/data.js
@@ -0,0 +1,67 @@
+export  const data = {
+    'OrderNumber': 'FA-20187',
+    'IssueDate': '2018-07-23T00:00:00',
+    'PaymentMethod': 'SMS',
+    'User': {
+      'LastName': 'Andrei',
+      'FirstName': 'Nicora',
+      'Email': 'andrei@alphorm.com',
+      'Phone': '06655484545'
+    },
+    'BillingAddress': {
+      'AddressId': null,
+      'Company': 'Alphard Tech, ltd',
+      'VatNumber': '145',
+      'AddressLine1': '10, Dar lol test',
+      'AddressLine2': 'Fin Ja had Khina street',
+      'City': 'RABAT',
+      'CountryId': 0,
+      'Country': 'France',
+      'PostalCode': 27182,
+      'State': 'Grand Casablanca',
+      'AddressTypeId': 0
+    },
+    'PriceExcludingVat': 0,
+    'VatAmount': 0,
+    'TotalPrice': 400,
+    'Currency': 'EUR',
+    'Items': [
+      {
+        'Description': 'VMware vSphere 5 : Savoir installer, configurer et administrer',
+        'PriceExcludingVat': 176,
+        'VatAmount': 0,
+        'TotalPrice': 176
+      },
+      {
+        'Description': 'VMware Mirage 3.6 : Savoir installer, configurer et administrer',
+        'PriceExcludingVat': 144,
+        'VatAmount': 0,
+        'TotalPrice': 144
+      },
+      {
+        'Description': 'VMware vSphere 5 : Savoir installer, configurer et administrer',
+        'PriceExcludingVat': 176,
+        'VatAmount': 0,
+        'TotalPrice': 176
+      },
+      {
+        'Description': 'VMware Mirage 3.6 : Savoir installer, configurer et administrer',
+        'PriceExcludingVat': 144,
+        'VatAmount': 0,
+        'TotalPrice': 144
+      },
+      {
+        'Description': 'VMware vSphere 5 : Savoir installer, configurer et administrer',
+        'PriceExcludingVat': 176,
+        'VatAmount': 0,
+        'TotalPrice': 176
+      },
+      {
+        'Description': 'VMware Mirage 3.6 : Savoir installer, configurer et administrer',
+        'PriceExcludingVat': 144,
+        'VatAmount': 0,
+        'TotalPrice': 144
+      }
+    ]
+  }
+  
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 // import Login from '../Login/Login';
+import InvoiceGenerator from 'invoice-generator/src/invoiceGenerator'
+import { data } from './data'
 
 export default function App() {
    /*  const [token, setToken] = useState();
     if(!token) {
         return <Login setToken={setToken} />
       } */
-      
+
+    const downloadInvoice = (data) => {
+      InvoiceGenerator(data, 'test', 'download')
+    }
+
     return <>
       <div className="dashboard">
         {/**
@@ -16,6 +22,7 @@ export default function App() {
          * 
          * TODO : add invoice-generator module
         */}
+        <Button onClick={() => downloadInvoice(data)}>Download Invoice</Button>
       </div>
       </>
     
